18b4c2df8357ce11dcf8f5ddecfb228ad299da96
Jimmy Waern <jimmy.waern@hotmail.com>
2017-01-23 09:21:28 +0100

    Added tests for values divisible by 3 and 5.

--- TDD-Coca-Cola.Tester/CocaColaTest.cs
+++ TDD-Coca-Cola.Tester/CocaColaTest.cs
@@ -10,15 +10,35 @@ namespace TDD_Coca_Cola.Tester
 {
     public class CocaColaTest
     {
+        NumberManager manager = new NumberManager();
+
         [Test]
         public void Input1Get1()
         {
-            var manager = new NumberManager();
+            //var manager = new NumberManager();
             string actual = manager.CocaColaGenerator(1);
             var expected = "1";
             Assert.AreEqual(actual, expected);
+        }
 
+        [Test]
 
+        //  Tests values between 1 and 15 thats divisible by 3.
+        public void Input3GetCoca(
+            [Values(3, 6, 9, 12)] int input)
+        {
+            string actual = manager.CocaColaGenerator(input);
+            Assert.AreEqual("Coca", actual);
+        }
+
+        [Test]
+
+        //  Tests values between 1 and 15 thats divisible by 5.
+        public void Input3GetCola(                      
+            [Values(5, 10)] int input)
+        {
+            string actual = manager.CocaColaGenerator(input);
+            Assert.AreEqual("Cola", actual);
         }
     }
 }
